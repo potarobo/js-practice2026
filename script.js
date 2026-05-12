431d937f064dbba8db56a7f8088a1f74244859e6
koji_potarobo <potarobo@gmail.com>
2026-05-12 16:23:47 +0900

    コメントforEach

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-const input = document.querySelector('#input');
-const list = document.querySelector('#list');
-const btn = document.querySelector('#btn');
+const items = document.querySelectorAll('.item');
 
-btn.addEventListener('click', function () {
-  const deleteBtn = document.createElement('button');
-  deleteBtn.textContent = 　'削除';
-  const li = document.createElement('li');
-  li.textCon　tent = input.value;
-  li.appendChild(deleteBtn);
-  list.appendChild(li);
-
-  deleteBtn.addEventListener('click', function () {
-    li.remove();
-  });
+items.forEach(function(item){
+  item.style.color = 'tomato' ;
 });
